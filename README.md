d96d7d397a8d7affac4c8405367b77fa3b0c8e5b
jimtin <jameshin84@gmail.com>
2022-11-11 15:03:31 +1100

    Updated README.md

--- README.md
+++ README.md
@@ -5,10 +5,14 @@ A trading bot developed by [Creative Appnologies](https://www.creativeappnologie
 This is an ongoing development project, please feel free to contribute your own thoughts. 
 
 ## Tutorials
-### How to Build a Crypto Trading Bot with Coinbase and Python
-#### Episodes
+### Coinbase Tutorials
+#### How to Build a Crypto Trading Bot with Coinbase and Python Series
 1. [How to Setup Your Crypto Trading Bot for Coinbase with Python](https://medium.com/@appnologyjames/how-to-connect-to-coinbase-with-python-3-97cf53856fcd). Explains how to set up your code in an extensible format.
 2. [How to Connect to Coinbase with Python 3](https://medium.com/@appnologyjames/how-to-connect-to-coinbase-with-python-3-97cf53856fcd). Shows you how to connect to the Coinbase Pro API.
 3. [How to Identify Engulfing Candles on Coinbase with Python](https://medium.com/@appnologyjames/how-to-identify-engulfing-candles-on-coinbase-with-python-27c6db4eda57). Shows you how to use Python to detect [Bullish Engulfing Patterns](https://www.investopedia.com/terms/b/bullishengulfingpattern.asp) and [Bearish Engulfing Patterns](https://www.investopedia.com/terms/b/bearishengulfingp.asp) in candlesticks. Introduces an advanced detection method as well.
 4. [How to Implement the Engulfing Candle Strategy on Coinbase with Python](https://medium.com/@appnologyjames/how-to-implement-the-engulfing-candle-strategy-on-coinbase-with-python-23ea20dbb502). Shows you how to implement the [Engulfing Candle Strategy](https://www.thebalancemoney.com/engulfing-candle-day-trading-strategy-1030873) using Python and Coinbase Pro REST API
 
+### MetaTrader Tutorials
+1. [How to Connect to MetaTrader 5 with Python](https://appnologyjames.medium.com/how-to-connect-to-metatrader-5-with-python-8db3501cefff)
+2. How to Initialize a Symbol MetaTrader 5 with Python - link coming soon
+3. How to Retrieve 50,000 Candlesticks from MetaTrader 5 with Python
---
+++ metatrader_lib/mt5_interaction.py
@@ -0,0 +1,106 @@
+import MetaTrader5
+
+
+# Function to start Meta Trader 5 (MT5)
+def start_mt5(username, password, server, path):
+    # Ensure that all variables are the correct type
+    uname = int(username) # Username must be an int
+    pword = str(password) # Password must be a string
+    trading_server = str(server) # Server must be a string
+    filepath = str(path) # Filepath must be a string
+
+    # Attempt to start MT5
+    if MetaTrader5.initialize(login=uname, password=pword, server=trading_server, path=filepath):
+        # Login to MT5
+        if MetaTrader5.login(login=uname, password=pword, server=trading_server):
+            return True
+        else:
+            print("Login Fail")
+            quit()
+            return PermissionError
+    else:
+        print("MT5 Initialization Failed")
+        quit()
+        return ConnectionAbortedError
+
+
+# Function to initialize a symbol on MT5
+def initialize_symbols(symbol_array):
+    # Get a list of all symbols supported in MT5
+    all_symbols = MetaTrader5.symbols_get()
+    # Create an array to store all the symbols
+    symbol_names = []
+    # Add the retrieved symbols to the array
+    for symbol in all_symbols:
+        symbol_names.append(symbol.name)
+
+    # Check each symbol in symbol_array to ensure it exists
+    for provided_symbol in symbol_array:
+        if provided_symbol in symbol_names:
+            # If it exists, enable
+            if MetaTrader5.symbol_select(provided_symbol, True):
+                print(f"Sybmol {provided_symbol} enabled")
+            else:
+                return ValueError
+        else:
+            return SyntaxError
+
+    # Return true when all symbols enabled
+    return True
+
+
+# Function to convert a timeframe string in MetaTrader 5 friendly format
+def set_query_timeframe(timeframe):
+    # Implement a Pseudo Switch statement. Note that Python 3.10 implements match / case but have kept it this way for
+    # backwards integration
+    if timeframe == "M1":
+        return MetaTrader5.TIMEFRAME_M1
+    elif timeframe == "M2":
+        return MetaTrader5.TIMEFRAME_M2
+    elif timeframe == "M3":
+        return MetaTrader5.TIMEFRAME_M3
+    elif timeframe == "M4":
+        return MetaTrader5.TIMEFRAME_M4
+    elif timeframe == "M5":
+        return MetaTrader5.TIMEFRAME_M5
+    elif timeframe == "M6":
+        return MetaTrader5.TIMEFRAME_M6
+    elif timeframe == "M10":
+        return MetaTrader5.TIMEFRAME_M10
+    elif timeframe == "M12":
+        return MetaTrader5.TIMEFRAME_M12
+    elif timeframe == "M15":
+        return MetaTrader5.TIMEFRAME_M15
+    elif timeframe == "M20":
+        return MetaTrader5.TIMEFRAME_M20
+    elif timeframe == "M30":
+        return MetaTrader5.TIMEFRAME_M30
+    elif timeframe == "H1":
+        return MetaTrader5.TIMEFRAME_H1
+    elif timeframe == "H2":
+        return MetaTrader5.TIMEFRAME_H2
+    elif timeframe == "H3":
+        return MetaTrader5.TIMEFRAME_H3
+    elif timeframe == "H4":
+        return MetaTrader5.TIMEFRAME_H4
+    elif timeframe == "H6":
+        return MetaTrader5.TIMEFRAME_H6
+    elif timeframe == "H8":
+        return MetaTrader5.TIMEFRAME_H8
+    elif timeframe == "H12":
+        return MetaTrader5.TIMEFRAME_H12
+    elif timeframe == "D1":
+        return MetaTrader5.TIMEFRAME_D1
+    elif timeframe == "W1":
+        return MetaTrader5.TIMEFRAME_W1
+    elif timeframe == "MN1":
+        return MetaTrader5.TIMEFRAME_MN1
+
+
+# Function to query previous candlestick data from MT5
+def query_historic_data(symbol, timeframe, number_of_candles):
+    # Convert the timeframe into an MT5 friendly format
+    mt5_timeframe = set_query_timeframe(timeframe)
+    # Retrieve data from MT5
+    rates = MetaTrader5.copy_rates_from_pos(symbol, mt5_timeframe, 1, number_of_candles)
+    return rates
